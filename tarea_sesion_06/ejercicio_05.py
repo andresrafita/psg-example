aeafd2e1a1907cd3af65e200bd781eaf21a7c221
Andres Rafael <andres.111.art@gmail.com>
2024-03-10 09:50:52 -0400

    ejercicio_05

---
+++ tarea_sesion_06/ejercicio_05.py
@@ -0,0 +1,11 @@
+#Ejercicio5
+
+#Compara y comprueba
+numero1 = 123
+numero2 = 890
+
+if numero1 % 2 == numero2 % 2:
+    print("Ambos números tienen la misma paridad (ambos pares o ambos impares).")
+else:
+    print("Los números tienen diferente paridad.")
+
