96825dad44d02d6314343290746e97fbce7717b0
Andres Rafael <andres.111.art@gmail.com>
2024-03-10 09:51:45 -0400

    ejercicio_06

---
+++ tarea_sesion_06/ejercicio_06.py
@@ -0,0 +1,13 @@
+#Ejercicio6
+
+# cuadrado de 123 en un rango determinado
+resultado = 123 ** 2
+if 0 < resultado < 20000:
+    print("El cuadrado de 123 se encuentra en el rango (0, 20000).")
+else:
+    print("El cuadrado de 123 no se encuentra en el rango (0, 20000).")
+
+#Ejercicio7
+    
+#cuadrado y suma de 2 y 2 
+print(2**2 == 2 + 2)
