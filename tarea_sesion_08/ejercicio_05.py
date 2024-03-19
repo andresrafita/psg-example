7a9606aafc206e81d2a41e7fd32bc997fea76d53
Andres Rafael <andres.111.art@gmail.com>
2024-03-18 21:51:38 -0400

    ejercicio_05

--- tarea_sesion_08/ejercicio_05.py
+++ tarea_sesion_08/ejercicio_05.py
@@ -3,16 +3,15 @@
 # Pedro, 591546324, La Paz
 # Maria, 591222421, El Alto
 # Sol, 591753159, Oruro
-#Genera una tupla anidada e imprime el resultado
+# Genera una tupla anidada e imprime el resultado
 
-guia_telefonica = [
-    ("Juan", (591123456, "La Paz")),
-    ("Pedro", (591546324, "La Paz")),
-    ("Maria", (591222421, "El Alto")),
-    ("Sol", (591753159, "Oruro"))
-]
+guia_telefonica = {
+    'Juan': ['591123456', 'La Paz'],
+    'Pedro': ['591546324', 'La Paz'],
+    'Maria': ['591222421', 'El Alto'],
+    'Sol': ['591753159', 'Oruro']
+}
 
-print("Guía Telefónica:")
-print(guia_telefonica)
-
+tupla_guia_telefonica = tuple(guia_telefonica.items())
+print(tupla_guia_telefonica)
 
